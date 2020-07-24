842fa39a7889501ee92cd0bcd563c149f7192bf8
Kony91BC <mordechay91@gmail.com>
2020-07-24 08:24:01 +0200

    spritesheet update, added workshop exterior, deleted reduntant files

---
+++ Aseprite/Enviro&Furniture_Spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="Enviro&amp;Furniture_Spritesheet" tilewidth="8" tileheight="8" tilecount="2500" columns="50">
+ <image source="K:/Enviro&amp;Furniture_Spritesheet.png" width="400" height="400"/>
+</tileset>
